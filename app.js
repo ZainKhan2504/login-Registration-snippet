031672d2440cc2f258251b68a709a22f5bf2bc79
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-12-30 16:09:10 +0500

    Register user

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
@@ -19,6 +21,28 @@ mongoose
 // EJS Middleware
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+
+// Bodyparser Middleware
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session Middleware
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Connect Flash Middleware
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 
 // Routes
 app.use("/", require("./routes/index"));
